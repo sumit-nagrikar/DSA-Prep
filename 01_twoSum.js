118d4c68b7dc260fd3ffdb25f6bd76a88679c641
sumitnagrikar <sumitnagrikar1@gmail.com>
2024-09-30 00:56:26 +0530

    Added info

---
+++ 01_twoSum.js
@@ -0,0 +1,39 @@
+/* Problem:
+You are given an array of numbers and a target sum. You need to find two numbers in the array whose sum equals the target and return their indices.
+
+Approach:
+To make this solution faster (from O(n²) to O(n)), we use a hash map (an object in JavaScript) to store numbers we've seen and their positions as we go through the array. */
+
+/* Brute force approach
+var twoSum = function(nums, target) {
+    for(let i=0; i<nums.length-1; i++){
+        for(let j=i+1;j<nums.length; j++){
+            if(nums[i]+nums[j] == target){
+                return [i,j];
+            }
+        }
+    }
+};
+}; */
+
+
+function twoSum(target,nums){
+    let map = {}// Hash map to store value and index
+
+    for(let i=0; i<nums.length; i++){
+        let complement = target - nums[i]
+
+        // Check if the complement is in the map
+        if(map[complement] !== undefined){
+            return [map[complement], i]// Return indices if found
+        }
+
+        //adding element to map to track the complemnt from previous elments and get their index
+        map[nums[i]] = i  
+    }
+}
+
+let target = 9;
+let nums = [2,7,3,4,5];
+
+console.log(twoSum(target,nums));
